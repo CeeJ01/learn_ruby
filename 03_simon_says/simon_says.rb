700a1061660fec1b9fc69c8e6db5f43b3b5f2cbc
ceej01 <ceej.carmean@tuta.io>
2018-02-05 15:48:00 -0700

    Finish exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(string)
+	string
+end
+
+def shout(string)
+	string.upcase
+end
+
+def repeat(string, n = 2)
+	([string] * n).join(" ")
+end
+
+def start_of_word(string, n)
+	string[0,n]
+end
+
+def first_word(string)
+	string.split.first
+end
+
+def titleize(string)
+	string.capitalize!
+	words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+	string.split(" ").map {|word| 
+        if words_no_cap.include?(word) 
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ")
+end
