eef261e6b31b8071637ba77717e1097b67783e98
ceej01 <ceej.carmean@tuta.io>
2018-02-05 13:13:47 -0700

    Finih exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(a,b)
+	a + b
+end
+
+def subtract(a,b)
+	a - b
+end
+
+def sum(numbers)
+	numbers.inject(0, :+)
+end
+
+def multiply(numbers)
+	numbers.inject(1, :*)		
+end
+
+def power(x, n)
+	if (n == 1)
+		return x
+	else
+		x = x * power(x, n-1)
+	end
+end
+
+def factorial(x)
+	if x <= 1
+		1
+	else
+		x * (factorial(x -1))
+	end
+end
