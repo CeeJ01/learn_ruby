db439a877378a64a49f978b9296178c4ef7cfe33
ceej01 <ceej.carmean@tuta.io>
2018-02-07 16:50:43 -0700

    finish exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+def translate(phrase)
+	#convert whole phrase to word array
+	words = phrase.split(' ')
+	words.each do |word|	
+		while (word[0] != 'a' || word[0] != 'e' || word[0] != 'i' || word[0] != 'o' || word[0] != 'u' )
+			word << word[0]
+			word[0] = ''
+		end
+		word << "ay"
+	end
+	return words[0]
+end
